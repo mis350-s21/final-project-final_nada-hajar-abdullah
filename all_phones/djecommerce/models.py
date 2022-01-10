9386aff0293c268cf824efd090c61e07fd2fee2c
hajerayed9 <hajer.salman@cba.ku.edu.kw>
2022-01-10 21:21:04 +0300

    haje

--- all_phones/djecommerce/models.py
+++ all_phones/djecommerce/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+ #Create your models here.
 class Customer(models.Model):
     user= models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
     name=models.CharField(max_length=200,null=True)
