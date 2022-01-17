7265bf04737a001bb35864aef3a33b0b0a73624d
Abdullah Tareq <abdullah.mohammad02@cba.ku.edu.kw>
2022-01-17 23:26:52 +0300

    I've done log in
    I have created an accounts to let the customer to log in in our web

--- all_phones/ECOMMERCE/settings.py
+++ all_phones/ECOMMERCE/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'djecommerce',
     
-    
+   
 ]
 
 MIDDLEWARE = [
@@ -134,3 +138,16 @@ X_FRAME_OPTIONS = '*'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = "/shop"
+ACCOUNT_EMAIL_VERIFICATION = "none"
